196b0566548a7e86b2a1cab2ca19b1229e4437ff
BenitaMichael <benubiaka@gmail.com>
2025-01-08 04:19:15 +0100

    feat:  manage category functionality

--- client/src/app/_components/addRecipient.js
+++ client/src/app/_components/addRecipient.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import styles from '../styles/components.module.css';
 
-const AddRecipient = () => {
+const addRecipient = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-  const [category, setCategory] = '';
+  const [category, setCategory] = useState('');
   const [message, setMessage] = useState(null);
 
   const handleSubmit = async (e) => {
@@ -33,7 +33,7 @@ const AddRecipient = () => {
   };
 
   return (
-    <div className= {styles.addRecipient}>
+    <div className={styles.addRecipient}>
       <h2 className={styles.componentHeader}>Add Recipient</h2>
       {message && <p className={styles.message}>{message}</p>}
       <form onSubmit={handleSubmit}>
@@ -63,7 +63,7 @@ const AddRecipient = () => {
             type="text"
             id="category"
             value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            onChange={(e) => setCategory(e.target.value)} // Correctly using setCategory
           />
         </div>
         <button type="submit" className={styles.formButton}>
@@ -74,4 +74,4 @@ const AddRecipient = () => {
   );
 };
 
-export default AddRecipient;
+export default addRecipient;
