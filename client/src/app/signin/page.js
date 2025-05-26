b92987465a78d565067f23ce83fb06642a009e58
BenitaMichael <benubiaka@gmail.com>
2025-05-26 13:25:30 +0100

    style: sign in UI complete

--- client/src/app/signin/page.js
+++ client/src/app/signin/page.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import styles from '../styles/components.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import mailImg from '../assets/Images/signMail.png'; 
 
 const SignInForm = () => {
   const [formData, setFormData] = useState({
@@ -25,41 +27,52 @@ const SignInForm = () => {
 
   return (
     <div className={styles.signupContainer}>
-      <div className={styles.imageContainer}>
       <h1 className={styles.componentHeader}>Welcome Back! Please sign in</h1>
-      <form className={styles.formGroup} onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className={styles.input}
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className={styles.input}
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit" className={styles.primaryButton}>
-          Sign In
-        </button>
-      </form>
-
-      <div className={styles.flexText}>
-        <p>Don’t have an account?</p>
-        <Link href="/signup" className={styles.buttonLink}>
-          <button className={styles.buttonLink}>
-            Register
+        <div className={styles.flexTextImage}>
+          <section>
+          <form className={styles.formGroup} onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className={styles.input}
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className={styles.input}
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" className={styles.primaryButton}>
+            Sign In
           </button>
-        </Link>
-      </div>
-      </div>
+        </form>
+
+        <div className={styles.flexText}>
+          <p>Don’t have an account?</p>
+          <Link href="/signup" className={styles.buttonLink}>
+            <button className={styles.buttonLink}>
+              Register
+            </button>
+          </Link>
+        </div>
+        </section>
+        <section>
+          <Image
+            src={mailImg}
+            alt="Sign In Image"
+            width={500}
+            height={500}
+            className={styles.mailImg}
+            />
+        </section>
+        </div>
     </div>
   );
 };
